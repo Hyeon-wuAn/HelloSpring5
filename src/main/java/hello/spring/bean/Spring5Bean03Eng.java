a7e5c56b4d8e6df9746a7d037aa9fd28e5d6bfe2
Hyeon-Wu An <zerox2519@gmail.com>
2023-06-23 14:51:55 +0900

    ..

--- src/main/java/hello/spring/bean/Spring5Bean03Eng.java
+++ src/main/java/hello/spring/bean/Spring5Bean03Eng.java
@@ -3,7 +3,7 @@ package hello.spring.bean;
 public class Spring5Bean03Eng implements Spring5Bean03{
 
     public void sayHello(String msg){
-        System.out.println("안녕하세요, " + msg);
+        System.out.println("Hello, " + msg);
     }
 
 }
